a1280c528d9d4baf74ae0e26ab644ede85eb306e
y0915060623 <102035442+y0915060623@users.noreply.github.com>
2022-03-31 11:48:04 +0800

    Add files via upload

---
+++ homework0330_1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+def sort1():
+    data=list()
+    a=int(input('請輸入a值: '))
+    b=int(input('請輸入b值: '))
+    c=int(input('請輸入c值: '))
+    data.append(a)
+    data.append(b)
+    data.append(c)
+    data.sort(reverse=True)
+    print(data)
+sort1()    
+       
+
